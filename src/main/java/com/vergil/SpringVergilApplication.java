8b9369e7a4350943f6c9f0b478d5937ab75b8b71
Alexander <mysq1@gmail.com>
2020-04-07 22:01:30 +0800

    首次提交

---
+++ src/main/java/com/vergil/SpringVergilApplication.java
@@ -0,0 +1,13 @@
+package com.vergil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVergilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVergilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vergil/SpringVergilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vergil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVergilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
